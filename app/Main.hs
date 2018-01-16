aefc333414eb62c1a2d700946e5e3114b1e45315
杉山希望 <nontan4061@gmail.com>
2018-01-16 23:35:55 +0900

    bug

--- app/Main.hs
+++ app/Main.hs
@@ -6,11 +6,40 @@ import qualified Data.ByteString.Char8 as S8
 import qualified Data.Yaml             as Yaml
 import           Network.HTTP.Simple
 
+responseStatusCodeIsSuccess :: Integral p => p -> Bool
+responseStatusCodeIsSuccess responseStatusCode
+    | code >= 200 && code < 300 = True
+    | otherwise                 = False
+    where
+        code = fromIntegral responseStatusCode
+
+-- -- getResponseStatusCodeFromResponse :: Response ->
+getResponseStatusCodeFromURL = ((responseStatusCodeIsSuccess . getResponseStatusCode) <*> httpJSON) . parseRequest_ 
+
+-- testAPIFromURL :: Monad m => [Char] -> m Bool
+testAPIFromURL url = getResponseStatusCodeFromURL url
+
+testURLList :: [[Char]]
+testURLList = [
+        "https://is09api.acqq.xyz/api/products"  ,
+        "https://is09api.acqq.xyz/api/products/1",
+        "https://is09api.acqq.xyz/api/products/2",
+        "https://is09api.acqq.xyz/api/products/3",
+        "https://is09api.acqq.xyz/api/products/4",
+        "https://is09api.acqq.xyz/api/products/5",
+        "https://is09api.acqq.xyz/api/products/6",
+        "https://is09api.acqq.xyz/api/products/7",
+        "https://is09api.acqq.xyz/api/products/8"
+    ]
+
 main :: IO ()
 main = do
-    response <- httpJSON "https://is09api.acqq.xyz/api/products"
+    print $ map getResponseStatusCodeFromURL testURLList
 
-    putStrLn $ "The status code was: " ++
-               show (fromIntegral (getResponseStatusCode response) / 200)
-    print $ getResponseHeader "Content-Type" response
-    S8.putStrLn $ Yaml.encode (getResponseBody response :: Value)
+    -- response <- httpJSON "https://is09api.acqq.xyz/api/products"
+
+    -- putStrLn $ "The status code was: " ++
+    --            show (fromIntegral (getResponseStatusCode response) / 200)
+    -- print $ getResponseHeader "Content-Type" response
+    -- S8.putStrLn $ Yaml.encode (getResponseBody response :: Value)
+
